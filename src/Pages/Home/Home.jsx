cc56f58bbdfe02f5da521b6200bdf044adc9e08f
soufo25 <nteyouyousouf@gmail.com>
2023-02-27 17:33:28 +0200

    ludo

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -7,7 +7,6 @@ const Home = () => {
   return (
     <div>
    <Slider />
-   <AboutUs />
    <Services />
     </div>
   )
--- src/Slider/Slider.jsx
+++ src/Slider/Slider.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import "react-responsive-carousel/lib/styles/carousel.min.css"; 
 import { Carousel } from 'react-responsive-carousel';
+import EmojiPeopleRoundedIcon from '@mui/icons-material/EmojiPeopleRounded';
+import GroupIcon from '@mui/icons-material/Group';
 
 import "./Slider.css";
 
@@ -10,8 +12,26 @@ const Slider = () => {
     
 
   return (
-    <div className='slider'>
-        <Carousel showThumbs={false} axis={'vertical'} infiniteLoop={true} autoPlay={true} interval={3000}>
+    <div className="container-main">
+    <div className="about-us">
+        <div className="topic">
+        <h2>
+          About Cash Afloat
+        </h2>
+        <p>
+          At Cash Afloat, we do more than just provide financial guide
+          to your future.
+
+          Since day one we have been guided bu our mission to help
+          families create tghe life they want to live, while protecting
+          their loved ones and planning their planning financial legacy
+        </p>
+        <button>
+        Get in touch!
+      </button>
+      </div>
+      <div className="slider">
+        <Carousel showThumbs={false} axis={'horizontal'} infiniteLoop={true} autoPlay={true} interval={3000}>
         <div className="image-con">
             <img src="./image/alexander-mils-lCPhGxs7pww-unsplash.jpg" alt='00'/>
             <div className="image-text">
@@ -43,7 +63,40 @@ const Slider = () => {
             </div>
         </div>
         </Carousel>
-    
+        </div>
+        </div>
+    <div className='experts-main'>
+      <div className="experts">
+        
+          <EmojiPeopleRoundedIcon />
+      
+          <h2>
+           Definitive Expertise
+          </h2>
+          <p>Our professionals are top experts with extensive
+            ,practical experience, we build strategies and systems </p>
+      </div>
+      <div className="experts">
+        
+          <GroupIcon />
+      
+          <h2>
+           Definitive Expertise
+          </h2>
+          <p>Our professionals are top experts with extensive
+            ,practical experience, we build strategies and systems </p>
+      </div>
+      <div className="experts">
+        
+          <EmojiPeopleRoundedIcon />
+      
+          <h2>
+           Definitive Expertise
+          </h2>
+          <p>Our professionals are top experts with extensive
+            ,practical experience, we build strategies and systems </p>
+      </div>
+    </div>
     </div>
   )
 }
